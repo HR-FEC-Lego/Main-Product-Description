77d591432eb50aedd2aae69f75ae630a4e7acd58
Scotty Cayton <kcayton34@gmail.com>
2021-01-14 11:25:25 -0700

    BasicApp running. Using React.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, 'client', 'src', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'client', 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [path.resolve(__dirname, 'client', 'src')],
+        exclude: [path.resolve(__dirname, 'node_modules'),
+          path.resolve(__dirname, 'server'),
+          path.resolve(__dirname, 'db'),
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      }
+    ],
+  }
+}
